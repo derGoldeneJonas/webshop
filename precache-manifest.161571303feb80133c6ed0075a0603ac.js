21c1412880b603ddf78c10914b260e202acd536c
derGoldeneJonas <jonas@bender-home.eu>
2021-07-07 19:24:46 +0200

    Updates

---
+++ precache-manifest.161571303feb80133c6ed0075a0603ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0146bace5bce997245f81303feca6b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eaceed5a84f5f7d5a99",
+    "url": "/static/js/2.2beb7579.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.2beb7579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c94d661c1220b6b0f68",
+    "url": "/static/js/main.ef069a78.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "ca1453fe3cec70570c0c2f42602aa855",
+    "url": "/static/media/Ernstings_family.ca1453fe.png"
+  }
+]);
