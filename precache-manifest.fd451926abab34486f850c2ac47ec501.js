c1a551cc2767adee52a2f640dcc7bc2089f29e37
derGoldeneJonas <jonas@bender-home.eu>
2021-07-08 18:42:47 +0200

    Updates

---
+++ precache-manifest.fd451926abab34486f850c2ac47ec501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc37d81f24548d9e7c541b4fabe66d7c",
+    "url": "/webshop/index.html"
+  },
+  {
+    "revision": "4eaceed5a84f5f7d5a99",
+    "url": "/webshop/static/js/2.2beb7579.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/webshop/static/js/2.2beb7579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523938ed4b8e7968dcd2",
+    "url": "/webshop/static/js/main.4a585934.chunk.js"
+  },
+  {
+    "revision": "1bb454b0e2ac8290d4c8",
+    "url": "/webshop/static/js/runtime-main.7360cb70.js"
+  },
+  {
+    "revision": "ca1453fe3cec70570c0c2f42602aa855",
+    "url": "/webshop/static/media/Ernstings_family.ca1453fe.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.161571303feb80133c6ed0075a0603ac.js"
+  "/webshop/precache-manifest.fd451926abab34486f850c2ac47ec501.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webshop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
